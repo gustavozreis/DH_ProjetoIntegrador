93f31faabecb5d642d3544b6c1574b14195c2a64
gustavozreis <gzreis@gmail.com>
2022-04-13 16:33:10 -0300

    Cria classe Character e classe Comic.
    Criação dos modelos das classes para os personagens e quadrinhos.

--- app/src/main/java/com/dhandroid2022/projetointegrador/ui/MainActivity.kt
+++ app/src/main/java/com/dhandroid2022/projetointegrador/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dhandroid2022.projetointegrador
+package com.dhandroid2022.projetointegrador.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dhandroid2022.projetointegrador.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

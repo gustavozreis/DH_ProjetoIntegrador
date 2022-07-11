870c4772af0a28ce03761e696ef250fed62487db
Tilin <guilherme_tilin@hotmail.com>
2022-07-11 13:02:32 -0300

    implementado novo logo implementado login com Google

--- app/src/main/java/com/dhandroid2022/projetointegrador/acess/data/UserRepository.kt
+++ app/src/main/java/com/dhandroid2022/projetointegrador/acess/data/UserRepository.kt
@@ -1,8 +1,16 @@
 package com.dhandroid2022.projetointegrador.acess.data
 
+
+
+
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class UserRepository {
@@ -19,4 +27,10 @@ class UserRepository {
         firebaseAuth.signInWithEmailAndPassword(email, password)
     }
 
+     fun googleAuthForFirebase (account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch {
+            firebaseAuth.signInWithCredential(credentials)
+        }
+    }
 }
